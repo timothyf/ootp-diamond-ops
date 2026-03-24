c8c7776a9a375718129eb79600f136c56a9a829e
Timothy Fisher <timothyf@gmail.com>
2026-03-24 17:20:08 -0400

    renamed strategy.py to generate.py

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python strategy.py --source db --db-url 'mysql+pymysql://root:@127.0.0.1:3306/ootp_db'
+python generate.py --source db --db-url 'mysql+pymysql://root:@127.0.0.1:3306/ootp_db'
